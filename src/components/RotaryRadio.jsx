be94c68c9ab3077ff79238ca060743ea71ddb851
Tiannanzhao <hello.tiannanzhao@gmail.com>
2026-04-09 16:18:55 +0100

    Increase grille rows from 14 to 20

--- src/components/RotaryRadio.jsx
+++ src/components/RotaryRadio.jsx
@@ -10,7 +10,7 @@ const STATIONS = [
 
 const SCRAMBLE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 const GRILLE_COLS = 18
-const GRILLE_ROWS = 14
+const GRILLE_ROWS = 20
 
 function useCurrentTime() {
   const [time, setTime] = useState(() => new Date())
